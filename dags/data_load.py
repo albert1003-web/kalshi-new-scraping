089c6c00663076b80844484d77a89265a3623244
albert1003-web <albertming88@gmail.com>
2026-04-21 14:19:26 -0500

    wipe tables, update with NYT scraping in DAG to SF

--- dags/data_load.py
+++ dags/data_load.py
@@ -19,7 +19,15 @@ with DAG(
     def get_kalshi_data():
         events = kalshi_scraping.get_all_kalshi_events()
         kalshi_scraping.save_to_csv(events)
-        return [("kalshi_data/events_keywords.csv", "EVENTS_KEYWORDS"), ("kalshi_data/kalshi_events.csv", "KALSHI_EVENTS"), ("keywords.csv", "KEYWORDS")]
+        return [("kalshi_data/events_keywords.csv", "EVENTS_KEYWORDS"), ("kalshi_data/kalshi_events.csv", "KALSHI_EVENTS")]
+    
+    @task 
+    def get_nyt_data():
+        events = NYT_scraping.get_recent_articles()
+        NYT_scraping.save_to_csv(events)
+        # load keywords.csv only once here since it has the all entries now
+        return [("nyt_data/nyt_articles.csv", "NY_TIMES_ARTICLES"), ("nyt_data/articles_keywords.csv", "ARTICLES_KEYWORDS"), ("keywords.csv", "KEYWORDS")]
+
 
     @task
     def load_file_to_stage(file_data):
@@ -28,18 +36,36 @@ with DAG(
         base = os.path.basename(filename)
         hook = SnowflakeHook(snowflake_conn_id='snowflake_default')
         path = f"/usr/local/airflow/include/data/{filename}"
-        put = f"PUT file://{path} @PUBLIC.KALSHI_STAGE"
-        copy_into = f"""COPY INTO {table} FROM @PUBLIC.KALSHI_STAGE/{base}.gz FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"')"""
+        put = f"PUT file://{path} @PUBLIC.KALSHI_STAGE OVERWRITE = TRUE"
+        copy_into = f"""COPY INTO {table} FROM @PUBLIC.KALSHI_STAGE/{base}.gz FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"') FORCE = TRUE"""
+        remove = f"REMOVE @PUBLIC.KALSHI_STAGE/{base}.gz"
         print(f"Loading {filename}...")
         hook.run(put)
         hook.run(copy_into)
+        hook.run(remove)
         return filename
+
+    @task
+    def merge_file_info(kalshi, nyt):
+        return kalshi + nyt
+
+    @task
+    def truncate_tables():
+        hook = SnowflakeHook(snowflake_conn_id='snowflake_default')
+        tables = ["EVENTS_KEYWORDS", "KALSHI_EVENTS", "KEYWORDS", "NY_TIMES_ARTICLES", "ARTICLES_KEYWORDS"]
+        for table in tables:
+            hook.run(f"TRUNCATE TABLE $[SNOWFALE_DATABASE].PUBLIC.{table}")
 
     @task(outlets=[raw_kalshi_dataset])
     def update_raw_kalshi_dataset():
         print("Raw Kalshi data was updated.")
 
-    file_info = get_kalshi_data()
+    truncated = truncate_tables()
+    kalshi_file_info = get_kalshi_data()
+    nyt_file_info = get_nyt_data()
+    truncated >> kalshi_file_info
+    truncated >> nyt_file_info
+    file_info = merge_file_info(kalshi_file_info, nyt_file_info)
     loaded_files = load_file_to_stage.expand(file_data=file_info)
     loaded_files >> update_raw_kalshi_dataset()
     
